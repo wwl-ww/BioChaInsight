7f70d20ee6866ffb706c2a63e4f1197bd4359ef6
Chi <159904337+wwl-ww@users.noreply.github.com>
2025-04-13 17:08:53 +0800

    加入split函数，用来分割实验组和对照组

--- BCarray.h
+++ BCarray.h
@@ -1,9 +1,9 @@
-# pragma once
+#pragma once
 
-# ifndef BCARRAY_H
-# define BCARRAY_H
+#ifndef BCARRAY_H
+#define BCARRAY_H
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 #include <vector>
 #include <functional>
@@ -17,24 +17,26 @@ template <typename T>
 class BCarray : public vector<T>
 {
 private:
-	bool is_row_vector = true;  // 是否是行向量
+    bool is_row_vector = true; // 是否是行向量
 
 public:
     // 构造函数
-    BCarray(const vector<T>& vec, bool is_row_vector = true) : vector<T>(vec), is_row_vector(is_row_vector){}
+    BCarray() : vector<T>(), is_row_vector(true) {}
 
-	BCarray(size_t count, const T& value = T(), bool is_row_vector = true) : vector<T>(count, value), is_row_vector(is_row_vector) {}
+    BCarray(const vector<T> &vec, bool is_row_vector = true) : vector<T>(vec), is_row_vector(is_row_vector) {}
+
+    BCarray(size_t count, const T &value = T(), bool is_row_vector = true) : vector<T>(count, value), is_row_vector(is_row_vector) {}
 
     BCarray(T array[], size_t size, bool is_row_vector = true)
     {
         this->assign(array, array + size);
-		this->is_row_vector = is_row_vector;
+        this->is_row_vector = is_row_vector;
     }
 
     bool isRowVector() const;
 
     /*统计函数*/
-   // 检查是否有重复值
+    // 检查是否有重复值
     bool hasDuplicates() const;
     // 计算和
     T sum() const;
@@ -69,41 +71,41 @@ public:
     // 众数
     T mode() const;
     // 计算协方差
-    double covariance(const BCarray<T>& vec, bool isSample = false) const;
+    double covariance(const BCarray<T> &vec, bool isSample = false) const;
     // 计算相关系数
-    double correlation(const BCarray<T>& vec) const;
+    double correlation(const BCarray<T> &vec) const;
     // 打印统计信息
     void info() const;
 
     /*一系列重载*/
-    friend ostream& operator<<<T>(ostream& os, const BCarray<T>& vec);
-    BCarray operator+(const BCarray<T>& vec2) const;
-    BCarray operator+(const T& scalar) const;
-    BCarray operator-(const BCarray<T>& vec2) const;
-    BCarray operator-(const T& scalar) const;
+    friend ostream &operator<< <T>(ostream &os, const BCarray<T> &vec);
+    BCarray operator+(const BCarray<T> &vec2) const;
+    BCarray operator+(const T &scalar) const;
+    BCarray operator-(const BCarray<T> &vec2) const;
+    BCarray operator-(const T &scalar) const;
     BCarray operator-() const;
-    BCarray operator*(const BCarray<T>& vec2) const;
-    BCarray operator*(const T& scalar) const;
-    BCarray operator/(const BCarray<T>& vec2) const;
-    BCarray operator/(const T& scalar) const;
-    BCarray operator&&(const BCarray<T>& vec2) const;
-    BCarray operator&&(const bool& scalar) const;
-    BCarray operator||(const BCarray<T>& vec2) const;
-    BCarray operator||(const bool& scalar) const;
+    BCarray operator*(const BCarray<T> &vec2) const;
+    BCarray operator*(const T &scalar) const;
+    BCarray operator/(const BCarray<T> &vec2) const;
+    BCarray operator/(const T &scalar) const;
+    BCarray operator&&(const BCarray<T> &vec2) const;
+    BCarray operator&&(const bool &scalar) const;
+    BCarray operator||(const BCarray<T> &vec2) const;
+    BCarray operator||(const bool &scalar) const;
     BCarray operator!() const;
 
     /*一些数学函数*/
     // 进行任意的自定义函数
     template <typename U>
-    BCarray<U> apply(function<U(const T&)>);
+    BCarray<U> apply(function<U(const T &)>);
     // 排序
     BCarray<T> sort(bool ascending = true);
     // BCarray<size_t> argsort(ascending = true);
     void sort_(bool ascending = true);
     // 内积
-    T dot(const BCarray<T>& vec);
+    T dot(const BCarray<T> &vec);
     // 转置。一般都是行向量，显示的.T()就会是列向量
-	BCarray<T> t() const;
+    BCarray<T> t() const;
 
     // 各种标准化
     BCarray<T> L2Normalize() const;
@@ -117,8 +119,10 @@ public:
     void zScoreNormalize_();
     void centralize_();
     void normalize_(string method = "L2");
-};
 
+    // 拆分实验组和对照组
+    pair<BCarray<T>, BCarray<T>> split(vector<int> &group);
+};
 
 // 实现
 template <typename T>
@@ -230,13 +234,13 @@ T BCarray<T>::mode() const
 }
 
 template <typename T>
-double BCarray<T>::covariance(const BCarray<T>& vec, bool isSample) const
+double BCarray<T>::covariance(const BCarray<T> &vec, bool isSample) const
 {
     return StatTools::covariance(*this, vec, isSample);
 }
 
 template <typename T>
-double BCarray<T>::correlation(const BCarray<T>& vec) const
+double BCarray<T>::correlation(const BCarray<T> &vec) const
 {
     return StatTools::correlation(*this, vec);
 }
@@ -263,7 +267,7 @@ void BCarray<T>::info() const
 }
 
 template <typename T>
-ostream& operator<<(ostream& os, const BCarray<T>& vec)
+ostream &operator<<(ostream &os, const BCarray<T> &vec)
 {
     os << "BCarray:\t";
     os << "[";
@@ -282,7 +286,7 @@ ostream& operator<<(ostream& os, const BCarray<T>& vec)
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator+(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator+(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -293,7 +297,7 @@ BCarray<T> BCarray<T>::operator+(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator+(const T& scalar) const
+BCarray<T> BCarray<T>::operator+(const T &scalar) const
 {
     BCarray<T> res(*this);
     for (size_t i = 0; i < res.size(); ++i)
@@ -304,7 +308,7 @@ BCarray<T> BCarray<T>::operator+(const T& scalar) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator-(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator-(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -315,7 +319,7 @@ BCarray<T> BCarray<T>::operator-(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator-(const T& scalar) const
+BCarray<T> BCarray<T>::operator-(const T &scalar) const
 {
     BCarray<T> res(*this);
     for (size_t i = 0; i < res.size(); ++i)
@@ -337,7 +341,7 @@ BCarray<T> BCarray<T>::operator-() const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator*(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator*(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -348,7 +352,7 @@ BCarray<T> BCarray<T>::operator*(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator*(const T& scalar) const
+BCarray<T> BCarray<T>::operator*(const T &scalar) const
 {
     BCarray<T> res(*this);
     for (size_t i = 0; i < res.size(); ++i)
@@ -359,7 +363,7 @@ BCarray<T> BCarray<T>::operator*(const T& scalar) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator/(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator/(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -370,7 +374,7 @@ BCarray<T> BCarray<T>::operator/(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator/(const T& scalar) const
+BCarray<T> BCarray<T>::operator/(const T &scalar) const
 {
     BCarray<T> res(*this);
     for (size_t i = 0; i < res.size(); ++i)
@@ -381,7 +385,7 @@ BCarray<T> BCarray<T>::operator/(const T& scalar) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator&&(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator&&(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -392,7 +396,7 @@ BCarray<T> BCarray<T>::operator&&(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator&&(const bool& scalar) const
+BCarray<T> BCarray<T>::operator&&(const bool &scalar) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -403,7 +407,7 @@ BCarray<T> BCarray<T>::operator&&(const bool& scalar) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator||(const BCarray<T>& vec2) const
+BCarray<T> BCarray<T>::operator||(const BCarray<T> &vec2) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -414,7 +418,7 @@ BCarray<T> BCarray<T>::operator||(const BCarray<T>& vec2) const
 }
 
 template <typename T>
-BCarray<T> BCarray<T>::operator||(const bool& scalar) const
+BCarray<T> BCarray<T>::operator||(const bool &scalar) const
 {
     BCarray<T> res(this->size());
     for (size_t i = 0; i < this->size(); ++i)
@@ -437,7 +441,7 @@ BCarray<T> BCarray<T>::operator!() const
 
 template <typename T>
 template <typename U>
-BCarray<U> BCarray<T>::apply(function<U(const T&)>)
+BCarray<U> BCarray<T>::apply(function<U(const T &)>)
 {
     BCarray<U> result(this->size());
     for (int i = 0; i < this->size(); i++)
@@ -455,9 +459,8 @@ template <typename T>
 BCarray<T> BCarray<T>::sort(bool ascending)
 {
     BCarray<T> result(*this);
-    sort(result.begin(), result.end(), [ascending](const T& a, const T& b) {
-        return ascending ? a < b : b < a;
-        });
+    sort(result.begin(), result.end(), [ascending](const T &a, const T &b)
+         { return ascending ? a < b : b < a; });
     return result;
 }
 
@@ -470,13 +473,12 @@ BCarray<T> BCarray<T>::sort(bool ascending)
 template <typename T>
 void BCarray<T>::sort_(bool ascending)
 {
-    sort(this->begin(), this->end(), [ascending](const T& a, const T& b) {
-        return ascending ? a < b : b < a;
-        });
+    sort(this->begin(), this->end(), [ascending](const T &a, const T &b)
+         { return ascending ? a < b : b < a; });
 }
 
 template <typename T>
-T BCarray<T>::dot(const BCarray<T>& vec)
+T BCarray<T>::dot(const BCarray<T> &vec)
 {
     return (*this * vec).sum();
 }
@@ -484,9 +486,9 @@ T BCarray<T>::dot(const BCarray<T>& vec)
 template <typename T>
 BCarray<T> BCarray<T>::t() const
 {
-	BCarray<T> result(*this);
-	result.is_row_vector = !this->is_row_vector;
-	return result;
+    BCarray<T> result(*this);
+    result.is_row_vector = !this->is_row_vector;
+    return result;
 }
 
 template <typename T>
@@ -538,7 +540,6 @@ BCarray<T> BCarray<T>::centralize() const
     result = result - meanValue;
     return result;
 }
-
 
 template <typename T>
 BCarray<T> BCarray<T>::normalize(string method) const
@@ -622,7 +623,7 @@ void BCarray<T>::normalize_(string method)
     {
         this->zScoreNormalize_();
     }
-    else if (method == "centralize") 
+    else if (method == "centralize")
     {
         this->centralize_();
     }
@@ -632,4 +633,26 @@ void BCarray<T>::normalize_(string method)
     }
 }
 
-# endif
+template <typename T>
+pair<BCarray<T>, BCarray<T>> BCarray<T>::split(vector<int> &group)
+{
+    BCarray<T> group1, group2;
+    for (int i = 0; i < group.size(); i++)
+    {
+        if (group[i] == 0)
+        {
+            group1.push_back(this->at(i));
+        }
+        else if (group[i] == 1)
+        {
+            group2.push_back(this->at(i));
+        }
+        else
+        {
+            throw std::invalid_argument("groupmust be 0 or 1.");
+        }
+    }
+    return make_pair(group1, group2);
+}
+
+#endif
